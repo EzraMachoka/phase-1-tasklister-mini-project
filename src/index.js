98ef9e990778a7c495381e043f28fec0567bc770
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-06-30 00:45:09 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.new_todo.value)
+    form.reset
+  })
 });
+
+
